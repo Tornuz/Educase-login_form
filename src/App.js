d3a64d593cb6d186f26d0c525eb61fb8d8f59885
Tornuz <yadavgourav2001@gmail.com>
2024-04-23 14:48:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './Components/Profile';
+import Home from './Components/Home';
+import Signin from './Components/Signin';
+import Signup from './Components/Signup';
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex align-items-end">
+     <BrowserRouter>
+     <Routes>
+     <Route
+          path="/signin"
+          element={<Signin />}
+        />
+     <Route
+          path="/signup"
+          element={<Signup />}
+        />
+     <Route
+          path="/profile"
+          element={<Profile />}
+        />
+        <Route
+          path="/"
+          element={<Home />}
+        />
+     <Route path="*" element={<Navigate to="/" />} />
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
